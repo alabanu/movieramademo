b523f14d6fdadcf47afdfc8ab5260844a3bee20e
a.alampanou <a.alampanou@insurance.nbg.gr>
2019-08-16 14:21:21 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,35 @@ let page = 0;
 const container = document.getElementById('container');
 const baseUrl = 'https://api.themoviedb.org/3/';
 
+$(document).ready(function() {
+ 
+       var stickyNavTop = $('.nav').offset().top;
+       
+       var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); 
+             
+        if (scrollTop > stickyNavTop) { 
+            $('.nav').addClass('sticky');
+        } else {
+            $('.nav').removeClass('sticky'); 
+        }
+    };
 
+    stickyNav();
+   
 window.onload = function(e){ 
     $(".loader").fadeIn("slow");
     addPage(++page);
 }
+
+window.onscroll = function () {
+    stickyNav();
+    if (getScrollTop() < getDocumentHeight() - window.innerHeight) return;
+    addPage(++page);
+    $(".loader").fadeIn("slow");
+};
+
+});
 
 /*In theaters*/
 function getDocumentHeight() {
@@ -29,12 +53,6 @@ function addPage(page) {
 }
 
 
-window.onscroll = function () {
-    if (getScrollTop() < getDocumentHeight() - window.innerHeight) return;
-    addPage(++page);
-    $(".loader").fadeIn("slow");
-};
-
 async function fetchPage(page) {
 
     url = baseUrl + 'movie/now_playing?api_key=bc50218d91157b1ba4f142ef7baaa6a0&page=' + page;
@@ -47,50 +65,56 @@ async function fetchPage(page) {
 
         array = JSON.stringify(myJson);
         var movies = JSON.parse(array);
-       
+        
+        
         for (var x = 0; x < movies.results.length; x++) {
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
+            card = document.createElement('li');
+            card.setAttribute('class', 'cards_item');
+
+            const carddiv = document.createElement('div');
+            carddiv.setAttribute('class', 'card');
 
             //title
             const h1 = document.createElement('h3');
             h1.textContent = movies.results[x].title;
-            card.appendChild(h1);
+            carddiv.appendChild(h1);
 
             //poster
+            const imagediv = document.createElement('div');
             var img = document.createElement("img");
-            img.setAttribute('class', 'img');
+            img.setAttribute('class', 'card_image');
             if (movies.results[x].poster_path != null) {
                 img.src = 'https://image.tmdb.org/t/p/original' + movies.results[x].poster_path;
             }
             else {
                 img.src = 'https://amfnews.com/wp-content/uploads/2014/10/default-img-1000x600.gif';
             }
-            card.appendChild(img);
+            imagediv.appendChild(img);
+            carddiv.appendChild(imagediv);
 
 
             //Release year
             const release_year = document.createElement('h5');
             release_year.innerHTML = getYear(movies.results[x].release_date);
-            card.appendChild(release_year);
+            carddiv.appendChild(release_year);
 
             //Genres
             const result = await getGenre(movies.results[x].genre_ids);
             const genres = document.createElement('div');
             genres.innerHTML = result;
-            card.appendChild(genres);
+            carddiv.appendChild(genres);
 
             //Vote average
             const vote_average = document.createElement('h5');
             vote_average.innerHTML = movies.results[x].vote_average;
-            card.appendChild(vote_average);
+            carddiv.appendChild(vote_average);
 
             //Overview
             const overview = document.createElement('p');
             overview.innerHTML = movies.results[x].overview;
-            card.appendChild(overview);
+            carddiv.appendChild(overview);
 
-            container.appendChild(card);
+            card.appendChild(carddiv);
             
             
 
@@ -101,9 +125,10 @@ async function fetchPage(page) {
             //add similar
             addSimilar(movies.results[x].id);
 
-
+            container.appendChild(card);
         }
         $(".loader").fadeOut("slow");
+        
 
     })
 
